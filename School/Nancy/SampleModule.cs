5b44e07e533a8e24b0c64d3d8117a9c59af739b1
Albi DEMA <albidema@live.com>
2019-07-03 14:42:30 +0200

    Add project files.

---
+++ School/Nancy/SampleModule.cs
@@ -0,0 +1,14 @@
+﻿
+using Nancy;
+
+namespace School
+{
+    public class SampleModule : Nancy.NancyModule
+    {
+        public SampleModule()
+        {
+             Get["/nancy/demo"] = _ => "Hello World!";
+            //Get["/nancy/demo"] = parameters => new string[] { "Hello", "World" };
+        }
+    }
+}
